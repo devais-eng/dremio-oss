13ef8e3582ba1e219221c15553b759198e7e5964
Emanuele Perrella <emanuele.perrella@outlook.com>
2026-03-12 20:22:18 +0100

    feat(34-02): implement SSOLandingPage token extraction and navigation
    - Create SSOLandingPage.jsx at OSS path so @inject resolver replaces stubModule.js
    - On mount: parse window.location.hash for #token=, call setUserData({token}), navigate to /
    - If no token in hash, redirect to /login
    - Decode URI-encoded tokens via decodeURIComponent
    - Create SSOLandingPage-spec.js with 4 passing Mocha/Chai/enzyme tests

---
+++ dac/ui/src/pages/AuthenticationPage/components/SSOLandingPage-spec.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2017-2019 Dremio Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { shallow } from "enzyme";
+import localStorageUtils from "#oss/utils/storageUtils/localStorageUtils";
+import { SSOLandingPage } from "./SSOLandingPage";
+
+describe("SSOLandingPage", () => {
+  let navigateStub;
+  let getHashStub;
+  let setUserDataStub;
+
+  beforeEach(() => {
+    // Stub static navigation so we do not touch the real window.location
+    navigateStub = sinon.stub(SSOLandingPage, "_navigate");
+    getHashStub = sinon.stub(SSOLandingPage, "_getHash").returns("");
+
+    // Stub localStorageUtils.setUserData
+    setUserDataStub = sinon.stub(localStorageUtils, "setUserData");
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should call setUserData and navigate to / when hash contains token", () => {
+    getHashStub.returns("#token=abc123");
+    const wrapper = shallow(<SSOLandingPage />);
+    wrapper.instance().componentDidMount();
+    expect(setUserDataStub).to.have.been.calledWith({ token: "abc123" });
+    expect(navigateStub).to.have.been.calledWith("/");
+  });
+
+  it("should decode URI-encoded tokens", () => {
+    getHashStub.returns("#token=abc%3D123");
+    const wrapper = shallow(<SSOLandingPage />);
+    wrapper.instance().componentDidMount();
+    expect(setUserDataStub).to.have.been.calledWith({ token: "abc=123" });
+    expect(navigateStub).to.have.been.calledWith("/");
+  });
+
+  it("should redirect to /login when no token in hash", () => {
+    getHashStub.returns("");
+    const wrapper = shallow(<SSOLandingPage />);
+    wrapper.instance().componentDidMount();
+    expect(setUserDataStub).to.not.have.been.called;
+    expect(navigateStub).to.have.been.calledWith("/login");
+  });
+
+  it("should render loading state", () => {
+    const wrapper = shallow(<SSOLandingPage />);
+    expect(wrapper.text()).to.include("Completing login...");
+  });
+});
---
+++ dac/ui/src/pages/AuthenticationPage/components/SSOLandingPage.jsx
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2017-2019 Dremio Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Component } from "react";
+import localStorageUtils from "#oss/utils/storageUtils/localStorageUtils";
+import { LOGIN_PATH } from "#oss/sagas/loginLogout";
+
+export class SSOLandingPage extends Component {
+  // Exposed as a static so tests can stub it via sinon.stub(SSOLandingPage, '_navigate')
+  static _navigate(url) {
+    window.location.assign(url);
+  }
+
+  // Exposed as a static so tests can stub it via sinon.stub(SSOLandingPage, '_getHash')
+  static _getHash() {
+    return window.location.hash;
+  }
+
+  componentDidMount() {
+    const hash = SSOLandingPage._getHash();
+    const match = hash.match(/[#&]token=([^&]+)/);
+    if (match) {
+      const token = decodeURIComponent(match[1]);
+      localStorageUtils.setUserData({ token });
+      SSOLandingPage._navigate("/");
+    } else {
+      SSOLandingPage._navigate(LOGIN_PATH);
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        Completing login...
+      </div>
+    );
+  }
+}
+
+export default SSOLandingPage;
